b663ee3c41e9fe2ecce8ef10fce4541841ac6bae
Ssegura09 <stephaniemsegura09@gmail.com>
2020-09-28 21:46:29 -0500

    travelers crud

--- app/controllers/travelers_controller.rb
+++ app/controllers/travelers_controller.rb
@@ -5,7 +5,46 @@ class TravelersController < ApplicationController
     end
 
     def show
-        @traveler = Traveler.find([:id])
+        @traveler = Traveler.find(params[:id])
     end
+
+    def new
+        @traveler = Traveler.new
+    end
+
+    def create
+        @traveler = Traveler.new(trav_params)
+        if @traveler.valid?
+          @traveler.save
+          redirect_to traveler_path(@traveler)
+        else
+          flash[:errors] = @traveler.errors.full_messages
+          redirect_to new_traveler_path
+        end
+      end
+
+      def edit
+        @traveler = Traveler.find(params[:id])
+      end
+    
+      def update
+        @traveler = Traveler.find(params[:id])
+        @traveler.update(trav_params)
+    
+        redirect_to @traveler
+      end
+
+      def destroy
+        @traveler = Traveler.find(params[:id])
+        @traveler.destroy
+    
+        redirect_to travelers_path
+      end
+
+    private
+    def trav_params
+        params.require(:traveler).permit(:email, :password, :first_name, :last_name, :age)
+    end
+
 
 end
